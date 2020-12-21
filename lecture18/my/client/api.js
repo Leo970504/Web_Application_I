6730952a2ca7b1839aa57b502607fdbf8e9d7e55
s273209@studenti.polito.it <s273209@studenti.polito.it>
2020-12-21 10:25:12 +0800

    6

---
+++ lecture18/my/client/api.js
@@ -0,0 +1,47 @@
+// All the API calls are defined here
+
+import Exam from "./exam.js";
+import Course from "./course.js";
+
+async function getAllExams() {
+    // call REST API : GET /exams
+    const response = await fetch('/exams');
+    const exams_json = await response.json();
+    if (response.ok) {
+        return exams_json.map((ex) => Exam.from(ex));
+    } else {
+        throw exams_json;  // An object with the error coming from the server
+    }
+}
+
+async function insertNewExam(exam) {
+    return new Promise((resolve, reject) => {
+        fetch('/exams', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(exam),
+        }).then( (response) => {
+            if(response.ok) {
+                resolve(null);
+            } else {
+                // analyze the cause of error
+                response.json()
+                .then( (obj) => {reject(obj);} ) // error msg in the response body
+                .catch( (err) => {reject({ errors: [{ param: "Application", msg: "Cannot parse server response" }] }) }); // something else
+                  }
+        }).catch( (err) => {reject({ errors: [{ param: "Server", msg: "Cannot communicate" }] }) }); // connection errors
+    });
+    
+}
+
+async function getAllCourses() {
+    const response = await fetch('/courses');
+    const jsoncourses = await response.json();
+    const courses = jsoncourses.map((jc) => Course.from(jc));
+    return courses;
+  }
+  
+
+export { getAllExams, insertNewExam, getAllCourses };
